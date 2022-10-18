f1603fdbbdea290a37900cb84301c430c11180c3
jakhongiralijonov <jakhongirweb@gmail.com>
2022-10-18 20:15:22 +0500

    increment added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,58 @@ import {Link} from 'react-router-dom'
 import { FaGraduationCap , FaUserGraduate, FaChalkboardTeacher,FaSmile} from 'react-icons/fa';
 import About from '../About';
 import Courses from '../Courses';
+import {useEffect,useState} from 'react'
 
 
 function Home() {
+
+const incrementBox =[
+   {
+      target: '10+',
+      title:'courses',
+      icon: <FaGraduationCap className='icon-box'/>,
+   },
+   {
+      target: '500+',
+      title:'students',
+      icon:<FaChalkboardTeacher className='icon-box'/>,
+
+   },
+   {
+      target: '10+',
+      title:'teachers',
+      icon:<FaUserGraduate className='icon-box'/>
+      
+   },
+   {
+      target: '100%',
+      title:'satisfaction',
+      icon: <FaSmile className='icon-box'/>
+   },
+]
+
+const [count, setCount] = useState(0);
+
+useEffect(
+        () => {
+            const timer = () => {
+                setCount(count + 1);
+            }
+
+            // if you want it to finish at some point
+            if (count >= 1100) {
+                return;
+            }
+            const id = setInterval(timer, 0.01);
+            return () => clearInterval(id);
+        },
+        [count]
+    );
+
+
+
+
+
   return (
     
 <section className="home" id="home">
@@ -28,40 +77,21 @@ function Home() {
 <section className="count">
 
    <div className="box-container">
-      <div className="box">
-<FaGraduationCap className='icon-box'/>
+      {
+         incrementBox.map((box)=>{
+            return (
+               <div className="box">
+               <div className="counter-box">{box.icon}</div>
          <div className="content">
-            <h3>10+</h3>
-            <p>courses</p>
+            <h3 className='counter' data-target="10">{count}</h3>
+            <p>{box.title}</p>
          </div>
       </div>
+            )
+         })
+      }
 
-      <div className="box">
-      <FaUserGraduate className='icon-box'/>
-         <div className="content">
-            <h3>1300+</h3>
-            <p>students</p>
-         </div>
-      </div>
-
-      <div className="box">
-      <FaChalkboardTeacher className='icon-box'/>
-
-         <div className="content">
-            <h3>10+</h3>
-            <p>teachers</p>
-         </div>
-      </div>
-
-      <div className="box">
-      <FaSmile className='icon-box'/>
-
-         <div className="content">
-            <h3>100%</h3>
-            <p>satisfaction</p>
-         </div>
-      </div>
-
+      
    </div>
 
 </section>
