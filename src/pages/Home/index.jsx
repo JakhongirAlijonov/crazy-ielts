05783cf0779fd1e2f7bba56bb8a9b31d6b45e074
jakhongiralijonov <jakhongirweb@gmail.com>
2022-10-21 22:49:17 +0500

    books section added and some home page changes made

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,28 @@ import './Style.css'
 import HomeImg from '../../assets/homg-img.svg'
 import {Link} from 'react-router-dom'
 import { FaGraduationCap , FaUserGraduate, FaChalkboardTeacher,FaSmile} from 'react-icons/fa';
-import About from '../About';
-import Courses from '../Courses';
+import AboutImg from '../../assets/about-img.svg'
+
+import CourseOne from '../../assets/course-1.svg'
+import coursesCards from '../Courses/coursesList'
+
 import {useEffect,useState} from 'react'
-import Teachers from '../Teachers';
 import Footer from '../../components/Footer';
-import Contact from '../Contact';
+import teachersList from '../Teachers/teachersList'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay} from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import SwiperCore from 'swiper';
+
+
+
+import { FaFacebook , FaInstagram, FaTelegramPlane} from 'react-icons/fa';
 
 
 function Home() {
+   SwiperCore.use([Autoplay])
 
 const incrementBox =[
    {
@@ -60,6 +73,7 @@ useEffect(
 
 
   return (
+
     
 <div>
 <section className="home" id="home">
@@ -100,13 +114,142 @@ useEffect(
 
 </section>
 
+<hr className="divider-hr" />
 
-<About/>
-<div className="quote">
-   Don't be lazy start today , not tomorrow!!!
+
+<section>
+
+   <>
+   <div className="row">
+
+<div className="image">
+
+
+<Swiper
+spaceBetween={50}
+slidesPerView={1}
+loop={true}
+autoplay
+
+>
+<SwiperSlide  className='about-slider-img'>
+<img src={AboutImg} alt="" />
+
+</SwiperSlide>
+<SwiperSlide className='about-slider-img slider-text'>
+<h1 className='about-slite-title'>Who we are ?</h1>
+<p className='about-slite-text'>We are a growing professional team of great teachers. We have been helping nearly <span className="highlighted">1000 students</span> to get at least <span className='highlighted'>5.5 IELTS</span> score by teaching English professionally. Our team is made up of great professionals who are confident in their pursuit of continuous improvement</p>
+
+</SwiperSlide>
+<SwiperSlide className='about-slider-img'>
+<img src={AboutImg} alt="" />
+
+</SwiperSlide>
+<SwiperSlide className='about-slider-img'>
+<img src={AboutImg} alt="biz haqimizda" />
+
+</SwiperSlide>
+...
+</Swiper>
+
+
+
+
+
 </div>
-<Courses/>
-<Teachers/>
+
+<div className="content">
+   <h3 className='about-title'>why choose us?</h3>
+   <p className='about-slite-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quibusdam magni error, aut enim rerum?</p>
+   <a href="#contact" class="btn">contact us</a>
+</div>
+
+</div>
+
+
+   </>
+</section>
+
+
+<p className="home-quote">Just Do It!</p>
+<hr className="divider-hr" />
+
+{/* Courses section */}
+<section className="courses" id="courses">
+
+<h1 className="heading">our <span>courses</span></h1>
+
+<Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      loop={true}
+      autoplay
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {
+        coursesCards.map(({title,img, about,cost,teacher})=>{
+          
+return (<SwiperSlide className='slide slide-card'>
+              
+  <img src={CourseOne} alt="course images" />
+  <h3 className='courses-title'>{title}</h3>
+  <p>{about}</p>
+  <div className="course-details">
+  <h2 className='course_teacher'>Teacher: <span className="teacher-span">{teacher}</span></h2>
+  <p className="course-cost">{cost}</p>
+  </div>
+  <a href="https://t.me/jakhongirbek_alijonov" className='enroll_course'>Enroll</a>
+          </SwiperSlide>
+        )})
+      }
+     
+    </Swiper>
+
+</section>
+
+
+
+<hr className="divider-hr" />
+
+<section className="teachers">
+<h1 className="heading">
+          expert <span>tutors</span>
+        </h1>
+
+        <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      loop={true}
+      autoplay={{ delay: 5000 }}
+      pagination={{clickable:true}}
+      
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {
+        teachersList.map((teacher)=>{
+          
+return (<SwiperSlide className='slide slide-card'>
+              
+<div key={teacher.id} className='teacher-card'>
+      <img src={HomeImg} alt="" />
+            <div className="share">
+            <a href={teacher.tg}><FaTelegramPlane/></a>
+            <a href={teacher}><FaFacebook/></a>
+            <a href={teacher.insta}><FaInstagram/></a>
+            </div>
+            <h3>{teacher.title}</h3>
+      </div>
+      ...
+          </SwiperSlide>
+        )})
+      }
+     
+    </Swiper>
+</section>
+<hr className="divider-hr" />
+
 </section>
 <Footer/>
 </div>
