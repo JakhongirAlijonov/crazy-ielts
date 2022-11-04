272b32b33ad1c544d5bdc61e9245912f51029cec
jakhongiralijonov <jakhongirweb@gmail.com>
2022-11-04 18:38:29 +0500

    blog section ready

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,8 +1,28 @@
 import './Style.css'
+import Heading from '../../components/Headings/headings'
+import WritingIdea from '../../assets/frog.jpg'
+import { Link } from 'react-router-dom'
+
 
 function Blog() {
   return (
-    <div>Blog</div>
+    <section className='blog_section'>
+    <Heading subChildren={'Blog'} titleChildren={'Useful contents'} />
+<div className="contents_wrapper">
+  <div className="content_card">
+    <img src={WritingIdea} alt="" />
+    <div className="content_texts">
+      <h1 className="content_title">
+      Writing part 1 sample
+      </h1>
+        <p className="content_desc">
+        The diagram illustrates the Life Cycle process of frogs in a pond.
+        </p>
+        <Link to='/article/1' className='btn article_btn' >Read article</Link>
+    </div>
+  </div>
+</div>
+    </section>
   )
 }
 
